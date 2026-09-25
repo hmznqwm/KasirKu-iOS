4cf06832690ce82b74046fbd2fa0a4b95b7c2663
hmznqwm <hmznqwm@gmail.com>
2026-09-25 23:37:20 +0700

    Fix undefined _projectId in NotificationService

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:kasirku_mobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const KasirKuApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
